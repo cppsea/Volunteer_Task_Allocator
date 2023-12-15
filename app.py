99db0c2dc3280faa52ae8704ef7d67bd4cc1d7d0
bearismybrother <thetwai123456789@gmail.com>
2023-12-14 16:38:26 -0800

    Getting Random Tasks Implemented

--- app.py
+++ app.py
@@ -3,34 +3,43 @@ from flask_sqlalchemy import SQLAlchemy
 import random
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' #location of the database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  #some sort of error here?
 db = SQLAlchemy(app)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-tasks =[{"task 1"}, {"task 2"},{"task 3"} ]
+tasks = ["Task 1", "Task 2", "Task 3", "a", "b", "c", "d", "e"]
+
+
 
 class Volunteer(db.Model):
-    name = db.Column(db.String, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, nullable=True)  #primary key automatically track and increment itself, can only have one
+    name = db.Column(db.String)
     taskAssigned= db.Column(db.String(200), nullable=False)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
 
-@app.route('/getTask', methods = ['POST', 'GET'])
+@app.route('/', methods = ['POST', 'GET'])
 def getTask():
     if request.method == 'POST':
         if tasks:
-            volunteerName = request.form['name'] #will have to pass in name in index html 26:50 in video
-            randomTask = Volunteer(name = volunteerName, taskAssigned=random.choice[tasks]) #add name and random task to database
-            tasks.remove(randomTask) #delete the task so it won't be picked again
+            volunteerName = request.form['content'] 
+            randomTask = Volunteer(name = volunteerName, taskAssigned=random.choice(tasks)) #add name and random task to database
+            tasks.remove(randomTask.taskAssigned) #delete the task so it won't be picked again
             db.session.add(randomTask)
             db.session.commit()
-        if not tasks:
-            return "No tasks left to do"
-    else 
+            volunteers= Volunteer.query.all()
+            return render_template("index.html", volunteers=volunteers)
+        else:
+            return render_template("noTasksLeft.html")  
+    else: 
         volunteers= Volunteer.query.all()
-        return render_template("index.html", volunteers=volunteers) #return to homepage to display the volunteers. 
-    
+        return render_template("index.html", volunteers=volunteers) 
 
+##@app.route('/Finished_Task/<string:name>', methods = ['POST', 'GET'])
     
+db.drop_all()
+db.create_all()
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
