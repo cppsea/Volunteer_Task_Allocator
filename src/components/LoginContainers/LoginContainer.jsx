c0443f0ec49d78f8cc0f7590b51023ebfa5c2576
Kevin Hernandez <kevdhz@gmail.com>
2023-12-22 11:39:56 -0800

    Update LoginContainer.jsx

--- src/components/LoginContainers/LoginContainer.jsx
+++ src/components/LoginContainers/LoginContainer.jsx
@@ -1,1 +1,15 @@
+function Front() {
+  
+  
+  
+  const [type, setType] = useState("signIn");
+  const handleOnClick = text => {
+    if (text !== type) {
+      setType(text);
+      return;
+    }
+  };
+  const containerClass =
+    "container " + (type === "signUp" ? "right-panel-active" : "");
+  
 
