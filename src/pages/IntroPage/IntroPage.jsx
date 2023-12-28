1e3ef6626699f417615e3d4abcb658baf8a5d407
Kevin Hernandez <kevdhz@gmail.com>
2023-12-28 14:01:49 -0800

    Create IntroPage.jsx

---
+++ src/pages/IntroPage/IntroPage.jsx
@@ -0,0 +1,52 @@
+import { useState} from 'react';
+import NameForm from "../../components/forms-LoginForm/LoginForm";
+import "./body.css";
+function Front() {
+
+  
+  
+  const [type, setType] = useState("signIn");
+  const handleOnClick = text => {
+    if (text !== type) {
+      setType(text);
+      return;
+    }
+  };
+  const containerClass =
+    "container " + (type === "signUp" ? "right-panel-active" : "");
+  
+  
+  
+return (
+  <div className="Front">
+      <h2>VTA CONCEPT</h2>
+      <div className={containerClass} id="container">
+        <NameForm />
+        <div className="overlay-container">
+          <div className="overlay">
+            <div className="overlay-panel overlay-left">
+              <h1>Welcome Back!</h1>
+              <p>
+                To login, please enter the listed information.
+              </p>
+              <button
+                className="ghost"
+                id="signIn"
+                onClick={() => handleOnClick("signIn")}
+              >
+                Log In
+              </button>
+            </div>
+            <div className="overlay-panel overlay-right">
+              <h1>Hello, Volunteer!</h1>
+              <p>Enter your personal details and get your task!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+      
+   
+  );
+  }
+export default Front;
