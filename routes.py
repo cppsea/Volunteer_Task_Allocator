85e44e5158ceac0331046a7ba8761407753042d1
Thet Wai <thetwai123456789.gmail.com>
2024-01-11 23:28:07 -0800

    Added get task and finish task, Haven't tested yet(not sure how to without html pages)

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from app import app
+from app import app, db
 from flask import (
     request,
     render_template,
@@ -25,3 +25,29 @@ def user(username):
     if current_user != User.query.filter_by(username=username).first():
         flash("You are not able to access this user page")
         redirect(url_for("index"))
+    #otherwise
+
+from models import Task
+import random
+@app.route("/user/get-random-task", methods = ["GET", "POST"])
+@login_required
+def get_task():
+    random_task = random.choice(Task.query.all())
+    current_user.tasks_assigned.append(random_task)
+
+    db.session.commit()
+
+    return render_template("Your-Tasks.html")
+
+@app.route("/user/finish-task<int:id>", methods = ["GET", "POST"])
+@login_required
+def finished_task(id):
+    task_finished = Task.query.get_or_404(id)
+    try:
+        userIDAssignedToTask= task_finished.person_assigned
+        if (userIDAssignedToTask == current_user.id):  #check if it's this request is from the same person who was assigned the task
+            current_user.tasks_assigned.remove(task_finished)
+            db.session.delete(task_finished)
+            db.session.commit()
+    except:
+        flash("Something wrong deleting")
