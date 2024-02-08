3c757c75010bf8b12c3f8f1e3096852c994f8f9b
daebreads <pbrian0819@gmail.com>
2024-02-08 15:49:31 -0800

    fixed import error by removing routes import

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from models import db
 from os import environ
-import routes
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
