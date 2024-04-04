642e9b517602369c849bfc37d577f867ae0a52b6
Darren52BT <darrenbt52@gmail.com>
2024-04-03 17:15:40 -0700

    created auth context provider and hook

--- client/src/pages/AdminPanels/AdminTasksPage.jsx
+++ client/src/pages/AdminPanels/AdminTasksPage.jsx
@@ -4,34 +4,35 @@ let test_tasks = [
   {
     id: 1,
     task: "Production Line/Restock",
-    shift:"Night",
+    shift: "Night",
     description:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque mollitia asperiores voluptas fuga. Quo, aut? Harum inventore dignissimos, voluptatibus iure, non quaerat, ipsa ea sapiente minus voluptatem officiis maiores necessitatibus?",
   },
   {
     id: 2,
     task: "Supervisor",
-    shift:"Morning",
+    shift: "Morning",
     description:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque mollitia asperiores voluptas fuga. Quo, aut? Harum inventore dignissimos, voluptatibus iure, non quaerat, ipsa ea sapiente minus voluptatem officiis maiores necessitatibus?",
   },
   {
     id: 3,
     task: "Check in ",
-    shift:"Afternoon",
+    shift: "Afternoon",
     description:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque mollitia asperiores voluptas fuga. Quo, aut? Harum inventore dignissimos, voluptatibus iure, non quaerat, ipsa ea sapiente minus voluptatem officiis maiores necessitatibus?",
   },
   {
     id: 4,
     task: "Media",
-    shift:"Night",
+    shift: "Night",
     description:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque mollitia asperiores voluptas fuga. Quo, aut? Harum inventore dignissimos, voluptatibus iure, non quaerat, ipsa ea sapiente minus voluptatem officiis maiores necessitatibus?",
   },
 ];
 export default function AdminTasksPage() {
   const [tasks, setTasks] = useState(test_tasks);
+
   return (
     <>
       <AdminTasks tasks={tasks} setTasks={setTasks} />
